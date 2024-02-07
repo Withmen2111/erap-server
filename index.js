4f6ff690b6898bb99e6951d937085bf6f7b3cf05
nabeel-dev1340 <nabeelsharafat@gmail.com>
2024-02-07 19:36:29 +0500

    increased payload size

--- index.js
+++ index.js
@@ -27,8 +27,11 @@ const web = new WebClient(slackToken);
 // Use the cors middleware
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Increase payload size limit to 10MB for JSON data
+app.use(express.json({ limit: "10mb" }));
+
+// Increase payload size limit to 10MB for URL-encoded data
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.post(
   "/postToSlack",
